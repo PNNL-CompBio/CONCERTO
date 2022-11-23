325bdeae936c3b9d125b6f5f0bbcbeede59e9cf0
James <james.ch.pino@gmail.com>
2022-11-23 15:17:16 -0500

    Adding changes in the way models are updated (to keep one instance of a model, with all code to get it to that point).
    Adding a few quick snippets to update model annotations. In order to get model tracking to work, i need to modify the models. This just add annotations.
    
    Making workflows act on specific branches only.

--- models/azotobacter_vinelandii_dj/change_model_compartment.py
+++ models/azotobacter_vinelandii_dj/change_model_compartment.py
@@ -10,38 +10,56 @@ models to see what the differences are.
 from memote.suite.cli.reports import diff
 import cobra
 import os
+import logging
+from models.helpers.id_mappers import update_ids
+
+log = logging.getLogger()
 
 _file_path = os.path.dirname(__file__)
 starting_model_f_name = 'iDT1278.xml'
 s_model_path = os.path.join(_file_path, starting_model_f_name)
 
-s_model = cobra.io.read_sbml_model(s_model_path)
-s_model.id = "AV"
+model = cobra.io.read_sbml_model(s_model_path)
+model.id = "AV"
+
+output_model_name = 'azo_vine.xml'
+output_model_path = os.path.join(_file_path, output_model_name)
 
 
-def modify_compartments(model):
-    new_model = model.copy()
-    for metabolite in new_model.metabolites:
+def write_model():
+    cobra.io.write_sbml_model(model, output_model_path)
+
+
+def update_1():
+    # updates bug in compartment of the model
+    log.info("Updating compartments")
+    for metabolite in model.metabolites:
         if metabolite.id[-2:] == "_u":
             # removes last two characters, namely "_u"
             metabolite.id = metabolite.id[:-2]
             # rename compartment
             metabolite.compartment = metabolite.id[-1]
-    return new_model
 
 
-av = modify_compartments(s_model)
-output_model_name = 'azo_vine.xml'
-output_model_path = os.path.join(_file_path, output_model_name)
-cobra.io.write_sbml_model(av, output_model_path)
-model_paths = [s_model_path, output_model_path]
+def update_2():
+    log.info('Adding annotations to metabolites')
+
+    update_ids(model)
+
+
+def update_model():
+    # Fix compartments
+    update_1()
+    update_2()
+    write_model()
 
 
 if __name__ == '__main__':
-
+    update_model()
+    model_paths = [s_model_path, output_model_path]
     diff(
         [
             *model_paths,
-            '--filename', os.path.join(_file_path, 'fix_compartment_diff.html')
+            '--filename', os.path.join(_file_path, 'model_differences.html')
          ]
     )
